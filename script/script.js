d036c722b4e6531cbb85120e8d560b31ef47ca1c
JonasMoeneclaey <jonas.moeneclaey@student.tsaam.be>
2025-11-21 12:14:59 +0100

    dit za het laatste zijn

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@
  */
 
 const kindSelector = document.querySelector("#kinderen")
+const SelGeschenk = document.querySelector("#geschenken")
+const DelGeschenk = document.querySelector("#DelGeschenken")
 
 fetch("https://o-apiandclient-render.onrender.com/kinderen")
   .then((info) => info.json()).then((kinderen) => {
@@ -11,6 +13,43 @@ fetch("https://o-apiandclient-render.onrender.com/kinderen")
         newOption.value = kind.id
         newOption.innerHTML = `${kind.voornaam} ${kind.achternaam}`
         kindSelector.appendChild(newOption)
+        
+    });
+  })
+
+fetch("https://o-apiandclient-render.onrender.com/geschenken")
+  .then((info) => info.json()).then((geschenken) => {
+    geschenken.forEach(geschenk => {
+        const newOption = document.createElement("option")
+        newOption.value = geschenk.id
+        newOption.innerHTML = geschenk.naam
+        SelGeschenk.appendChild(newOption)
+        
+    });
+  })
+
+  SelGeschenk.addEventListener("change", (e) => {
+    console.log(e.target.value)
+  })
+
+  kindSelector.addEventListener("change", (e) => {
+      const options = {
+        method: "...",
+        headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(),
+  };
+
+  })
+
+fetch("https://o-apiandclient-render.onrender.com/geschenken")
+  .then((info) => info.json()).then((geschenken) => {
+    geschenken.forEach(geschenk => {
+        const newOption = document.createElement("option")
+        newOption.value = geschenk.id
+        newOption.innerHTML = geschenk.naam
+        DelGeschenk.appendChild(newOption)
         
     });
   })
