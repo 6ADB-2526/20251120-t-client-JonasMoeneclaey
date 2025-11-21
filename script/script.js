f47a6b4647c39ae3d7542baf56daa75d36ef3eac
JonasMoeneclaey <jonas.moeneclaey@student.tsaam.be>
2025-11-21 11:53:03 +0100

    1 ding

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
 /**
  * 
  */
+
+const kindSelector = document.querySelector("#kinderen")
+
+fetch("https://o-apiandclient-render.onrender.com/kinderen")
+  .then((info) => info.json()).then((kinderen) => {
+    kinderen.forEach(kind => {
+        const newOption = document.createElement("option")
+        newOption.value = kind.id
+        newOption.innerHTML = `${kind.voornaam} ${kind.achternaam}`
+        kindSelector.appendChild(newOption)
+        
+    });
+  })
